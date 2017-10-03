5d39eaca1e602e67136db00c7e7bce0d8a14263a
longkey1 <longkey1@gmail.com>
2017-10-03 22:50:16 +0900

    Tryout sqlx

--- cli.go
+++ cli.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
+	"time"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/urfave/cli"
 )
 
@@ -26,10 +30,7 @@ func (c *CLI) Run(args []string) int {
 	app.Name = "epgrec-program-finder"
 	app.Version = "0.0.1"
 	app.Usage = "epgrec program finder"
-	app.Action = func(ctx *cli.Context) error {
-		fmt.Println("Hello world!")
-		return nil
-	}
+	app.Action = find
 
 	err := app.Run(args)
 	if err != nil {
@@ -38,4 +39,33 @@ func (c *CLI) Run(args []string) int {
 	}
 
 	return ExitCodeOK
+}
+
+// Program ...
+type Program struct {
+	Title     string    `db:"title"`
+	StartTime time.Time `db:"starttime"`
+}
+
+func find(ctx *cli.Context) error {
+	db, err := sqlx.Connect("mysql", "moto:motomoto@(192.168.0.40:3306)/epgrec?parseTime=true")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	programs := []Program{}
+	args := map[string]interface{}{
+		"category_id": 8,
+	}
+	nstmt, err := db.PrepareNamed("SELECT rp.title, rp.starttime FROM Recorder_programTbl AS rp LEFT OUTER JOIN Recorder_reserveTbl AS rr ON rr.program_id = rp.id WHERE rp.category_id = :category_id")
+	err = nstmt.Select(&programs, args)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, v := range programs {
+		fmt.Printf("%v\n", v)
+	}
+
+	return nil
 }
